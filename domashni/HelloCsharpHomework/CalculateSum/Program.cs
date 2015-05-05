99dc2a288d6cb2bb3cc226d0021e7eee8e012733
madoxxs <simeon_pri@abv.bg>
2015-05-05 18:07:46 +0300

    Tova e domashnoto mi !

---
+++ domashni/HelloCsharpHomework/CalculateSum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSum
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum of number ten and three is: "+(10+3));//tuk moje i bez skobi,no za po pregledno.
+        }
+    }
+}
---
+++ domashni/HelloCsharpHomework/ThisIsMyFirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsMyFirstProject
+{
+    class Info
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simeon Hristov Prisadov");
+            Console.WriteLine("05.05.2015");
+            Console.WriteLine(DateTime.Now);//ne bqh siguren kakvo si iskal da kajesh v uslovieto i zatova go napravih po dva nachina tova za datata.
+        }
+    }
+}
